cde5fd1e3c2ee7780f39e2c94bee4e5b3b9ae894
mengxz <mengxz188@qq.com>
2016-12-08 00:16:34 +0800

    commit -m 'test imu'

--- astroPi.py
+++ astroPi.py
@@ -36,7 +36,7 @@ class AstroPi(object):
 
     def get_heading(self):
         north = self.sense.get_compass()
-        offset =0
+        offset = 0
         return int((north + offset) % 360)
 
     def get_radians(self):
@@ -77,7 +77,31 @@ class AstroPi(object):
 sense = AstroPi()
 
 if __name__ == "__main__":
+    # while True:
+    #     sense.sense.clear()
+    #     sense.printInfo()
+    #     raw_input("Next")
+    from sense_hat import SenseHat
+
+    sense = SenseHat()
+    sense.clear()
+
+    edge = [0, 1, 2, 3, 4, 5, 6, 7, 15, 23, 31, 39, 47, 55,
+            63, 62, 61, 60, 59, 58, 57, 56, 48, 40, 32, 24, 16, 8]
+    length = len(edge)
+    ratio = length / 360.0
+
     while True:
-        sense.sense.clear()
-        sense.printInfo()
-        raw_input("Next")
+        o = sense.get_orientation()
+        pitch = o["pitch"]
+        roll = o["roll"]
+        yaw = o["yaw"]
+
+        yaw_list_position = int(yaw * ratio)
+
+        yaw_pixel_number = edge[yaw_list_position]
+
+        y = yaw_pixel_number // 8
+        x = yaw_pixel_number % 8
+
+        sense.set_pixel(x, y, 255, 255, 255)
